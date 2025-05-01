28f5dd632bfa74c84c95eb14de2f521546f30790
Priya-150 <159451684+Priya-150@users.noreply.github.com>
2025-05-01 06:43:24 -0700

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBTeZHFam5J_Nq16gvWK1b0Vu9i2W8YkLQ",
+  authDomain: "car-parking-9d210.firebaseapp.com",
+  projectId: "car-parking-9d210",
+  storageBucket: "car-parking-9d210.firebasestorage.app",
+  messagingSenderId: "543609389333",
+  appId: "1:543609389333:web:7d4b9b1f89f4a5b5cb291c",
+  measurementId: "G-VKFL8WJNTV"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
